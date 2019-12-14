8c860aae99fca20d85df05e6fd81c7525675d558
jvta1 <jvta1@LAPTOP-7UVMCO0K>
2019-12-14 17:00:19 +0530

    test

---
+++ src/main/java/Poc/BuyProduct.java
@@ -0,0 +1,24 @@
+package Poc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testleaf.base.ProjectSpecificMethods;
+import org.testng.annotations.Test;
+
+public class BuyProduct extends 
+	
+	public static void ProductPage() {
+		
+		
+		
+			
+			
+	
+	}
+
+}
